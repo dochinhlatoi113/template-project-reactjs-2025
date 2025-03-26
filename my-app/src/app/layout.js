75ccc37778961729524ad5141e147d8b343a30fe
Khuong Manh BUI <dochinhlatoi113@gmail.com>
2025-03-26 12:20:00 +0700

    updated

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 //web
 import "./css/web/header.css";
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
