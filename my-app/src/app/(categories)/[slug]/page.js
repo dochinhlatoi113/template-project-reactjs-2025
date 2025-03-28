c1cb5998c64a44888761a49f164968cfc8a9d0d0
bui khuong <dochinhlatoi113@gmail.com>
2025-03-28 22:05:47 +0700

    updated

--- my-app/src/app/(categories)/[slug]/page.js
+++ my-app/src/app/(categories)/[slug]/page.js
@@ -1,21 +1,31 @@
 "use client";
 import { use, useState } from "react";
+import styles from "../../(categories)/[slug]/pricebar.module.css";
 
 export default function CategoryPage({ params }) {
     const unwrappedParams = use(params);
     const [slug, setSlug] = useState(unwrappedParams.slug);
+    const [zIndexLeft,setZIndexLeft] = useState(0);
+    const [zIndexRight,setZIndexRight] = useState(0);
     const [minValue, setMinValue] = useState(0);
     const [maxValue, setMaxValue] = useState(100);
-
+    const [priceBarColor, setPriceBarColor] = useState('100%')
     const handleMinChange = (e) => {
+        setZIndexLeft(1);
+        setZIndexRight(0)
         const valueMin = Number(e.target.value);
+        console.log(minValue)
+        setPriceBarColor(valueMin)
         if (valueMin < maxValue - 5) {
             setMinValue(valueMin);
         }
     };
 
     const handleMaxChange = (e) => {
+        setZIndexLeft(0);
+        setZIndexRight(1)
         const valueMax = Number(e.target.value);
+        setPriceBarColor(maxValue)
         if (valueMax > minValue + 5) {
             setMaxValue(valueMax);
         }
@@ -53,14 +63,25 @@ export default function CategoryPage({ params }) {
                             />
                         </div>
                         <div className="pt-4 range_container">
-                            <div className="sliders_control">
-                                <input
-                                    className="absolute h-[10px] w-full pointer-events-none"
-                                    id="fromSlider" type="range" value={minValue} min="0" max="100" onChange={handleMinChange} />
-
-                                <input
-                                    className="absolute h-[10px] w-full bpointer-events-none"
-                                    id="toSlider" type="range" value={maxValue} min="0" max="100" onChange={handleMaxChange} />
+                            <div className={styles.sliders_control}>
+                                <div className="">
+                                    <input
+                                        className={`${styles.rangeSlider} left-[1%]`}
+                                        style={{
+                                            '--bg-color': `${(minValue / 100) * 100}%`, 
+                                            '--z-index-left': zIndexLeft,
+                                          }}
+                                     id={`${styles.fromSlider}`}  type="range" value={minValue} min="0" max="100" onChange={handleMinChange} />
+                                </div>
+                                <div className="">
+                                    <input
+                                        className={`${styles.rangeSlider} left-[14%] z-${zIndexRight}`}
+                                        style={{
+                                            '--bg-color': `${(maxValue / 100) * 100}%`, 
+                                            '--z-index': zIndexRight,
+                                          }}
+                                        id="toSlider" type="range" value={maxValue} min="0" max="100" onChange={handleMaxChange} />
+                                </div>
                             </div>
                         </div>
                     </div>
